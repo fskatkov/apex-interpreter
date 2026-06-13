f377717870e48118d2250da3bddfd229d62c38a6
Katkov Fedor <pweverj126@gmail.com>
2026-06-14 00:52:59 +0300

    [Feature][Stdlib] Expand the Array standard library
    Added clone, first, last, at, set, insertAt, removeAt methods to Array stdlib

--- src/stdlib/ArrayLib/ArrayBuiltins.cpp
+++ src/stdlib/ArrayLib/ArrayBuiltins.cpp
@@ -2,63 +2,123 @@
 
 namespace stdlib::ArrayBuiltins {
     static Value retrieveArraySize(Value receiver, const std::vector<Value> &args) {
-        if (!receiver.is<std::shared_ptr<Array>>()) {
-            throw std::runtime_error("`length` called on non-array receiver");
-        }
-
         return static_cast<double>(receiver.get<std::shared_ptr<Array>>()->size());
     }
 
-    static Value getElementByIndex(Value receiver, const std::vector<Value> &args) {
-        if (!receiver.is<std::shared_ptr<Array>>()) {
-            throw std::runtime_error("`at` called on non-array receiver");
-        }
-
-        if (!args[0].is<double>()) {
-            throw std::runtime_error("array index must be an integer");
-        }
-
-        const auto receivedObject = receiver.get<std::shared_ptr<Array>>();
-        const auto idx = static_cast<std::size_t>(args[0].get<double>());
-        return (*receivedObject)[idx];
-    }
-
     static Value clearArray(Value receiver, const std::vector<Value> &args) {
-        if (!receiver.is<std::shared_ptr<Array>>()) {
-            throw std::runtime_error("`clear` called on non-array receiver");
-        }
-
         const auto receivedObject = receiver.get<std::shared_ptr<Array>>();
         receivedObject->clear();
         return receivedObject;
     }
 
-    static Value appendElementToArrayEnd(Value receiver, const std::vector<Value> &args) {
-        if (!receiver.is<std::shared_ptr<Array>>()) {
-            throw std::runtime_error("`append` called on non-array receiver");
+    static Value createArrayClone(Value receiver, const std::vector<Value> &args) {
+        return receiver.get<std::shared_ptr<Array>>();
+    }
+
+    static Value getFrontArrayElement(Value receiver, const std::vector<Value> &args) {
+        const auto receivedObject = receiver.get<std::shared_ptr<Array>>();
+        if (receivedObject->empty()) {
+            throw std::runtime_error("no first element, an array is empty");
+        }
+        return receivedObject->front();
+    }
+
+    static Value getBackArrayElement(Value receiver, const std::vector<Value> &args) {
+        const auto receivedObject = receiver.get<std::shared_ptr<Array>>();
+        if (receivedObject->empty()) {
+            throw std::runtime_error("no last element, an array is empty");
+        }
+        return receivedObject->back();
+    }
+
+    static Value getElementByIndex(Value receiver, const std::vector<Value> &args) {
+        const auto receivedObject = receiver.get<std::shared_ptr<Array>>();
+
+        if (!args[0].is<double>()) {
+            throw std::runtime_error("array index must be an integer");
         }
 
+        const auto idx = static_cast<std::size_t>(args[0].get<double>());
+        return (*receivedObject)[idx];
+    }
+
+    static Value updateElementByIndex(Value receiver, const std::vector<Value> &args) {
+        auto receivedObject = receiver.get<std::shared_ptr<Array>>();
+
+        if (!args[0].is<double>()) {
+            throw std::runtime_error("array index must be an integer");
+        }
+
+        const auto idx = static_cast<std::size_t>(args[0].get<double>());
+
+        if (idx <= 0 || idx >= receivedObject->size()) {
+            throw std::runtime_error("array index out of bounds");
+        }
+
+        (*receivedObject)[idx] = args[1];
+        return receivedObject;
+    }
+
+    static Value appendElementToArrayEnd(Value receiver, const std::vector<Value> &args) {
         const auto receivedObject = receiver.get<std::shared_ptr<Array>>();
         receivedObject->push_back(args[0]);
         return NIL{};
     }
 
-    static Value removeElementFromArrayEnd(Value receiver, const std::vector<Value> &args) {
-        if (!receiver.is<std::shared_ptr<Array>>()) {
-            throw std::runtime_error("`pop` called on non-array receiver");
+    static Value appendElementByIndex(Value receiver, const std::vector<Value> &args) {
+        const auto receivedObject = receiver.get<std::shared_ptr<Array>>();
+
+        if (!args[0].is<double>()) {
+            throw std::runtime_error("array index must be an integer");
         }
 
+        const auto idx = static_cast<int>(args[0].get<double>());
+
+        if (idx <= 0 || idx >= receivedObject->size()) {
+            throw std::runtime_error("array index out of bounds");
+        }
+
+        receivedObject->insert(receivedObject->begin() + idx, args[1]);
+        return NIL{};
+    }
+
+    static Value removeElementFromArrayEnd(Value receiver, const std::vector<Value> &args) {
         const auto receivedObject = receiver.get<std::shared_ptr<Array>>();
         const auto finalElement = receivedObject->back();
         receivedObject->pop_back();
         return finalElement;
     }
 
+    static Value removeElementByIndex(Value receiver, const std::vector<Value> &args) {
+        const auto receivedObject = receiver.get<std::shared_ptr<Array>>();
+
+        if (!args[0].is<double>()) {
+            throw std::runtime_error("array index must be an integer");
+        }
+
+        const auto idx = static_cast<std::size_t>(args[0].get<double>());
+
+        if (idx <= 0 || idx >= receivedObject->size()) {
+            throw std::runtime_error("array index out of bounds");
+        }
+
+        receivedObject->erase(receivedObject->begin() + idx);
+        return NIL{};
+    }
+
     void registerMethods(std::unordered_map<std::string, std::shared_ptr<NativeFunction>>& registry) {
         registry["len"] = std::make_shared<NativeFunction>("len", 0, retrieveArraySize);
-        registry["at"] = std::make_shared<NativeFunction>("at", 1, getElementByIndex);
         registry["clear"] = std::make_shared<NativeFunction>("clear", 0, clearArray);
+        registry["clone"] = std::make_shared<NativeFunction>("clone", 0, createArrayClone);
+
+        registry["first"] = std::make_shared<NativeFunction>("first", 0, getFrontArrayElement);
+        registry["last"] = std::make_shared<NativeFunction>("last", 0, getBackArrayElement);
+        registry["at"] = std::make_shared<NativeFunction>("at", 1, getElementByIndex);
+        registry["set"] = std::make_shared<NativeFunction>("set", 2, updateElementByIndex);
         registry["append"] = std::make_shared<NativeFunction>("append", 1, appendElementToArrayEnd);
+        registry["insertAt"] = std::make_shared<NativeFunction>("insertAt", 2, appendElementByIndex);
         registry["pop"] = std::make_shared<NativeFunction>("pop", 0, removeElementFromArrayEnd);
+        registry["removeAt"] = std::make_shared<NativeFunction>("removeAt", 1, removeElementByIndex);
+
     }
 }
