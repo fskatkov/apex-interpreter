61f427dc1aa5063961f87cb754e3943e6dd0ab1a
Katkov Fedor <pweverj126@gmail.com>
2026-06-15 08:09:55 +0300

    [Feature][Stdlib] Updated Dictionary stdlib
    Finally, Dictionary stdlib consists of a complete set of basic methods

--- src/stdlib/DictionaryBuiltins/DictionaryBuiltins.cpp
+++ src/stdlib/DictionaryBuiltins/DictionaryBuiltins.cpp
@@ -20,9 +20,21 @@ namespace stdlib::DictionaryBuiltins {
         return receivedObject->empty();
     }
 
+    static Value containsValue(Value receiver, const std::vector<Value> &args) {
+        const auto receivedObject = receiver.get<std::shared_ptr<Dictionary>>();
+        if (!args[0].is<double>() && !args[0].is<std::string>()) {
+            throw std::runtime_error("dictionary key must be number or string");
+        }
+        return receivedObject->contains(args[0]);
+    }
+
     static Value getDictionaryElementByKey(Value receiver, const std::vector<Value> &args) {
         const auto receivedObject = receiver.get<std::shared_ptr<Dictionary>>();
         auto defaultValue = args.size() > 1 ? args[1] : NIL{};
+
+        if (!args[0].is<double>() && !args[0].is<std::string>()) {
+            throw std::runtime_error("dictionary key must be number or string");
+        }
 
         if (auto it = receivedObject->find(args[0]); it != receivedObject->end()) {
             return it->second;
@@ -33,6 +45,11 @@ namespace stdlib::DictionaryBuiltins {
 
     static Value setDictionaryElementByKey(Value receiver, const std::vector<Value> &args) {
         const auto receivedObject = receiver.get<std::shared_ptr<Dictionary>>();
+
+        if (!args[0].is<double>() && !args[0].is<std::string>()) {
+            throw std::runtime_error("dictionary key must be number or string");
+        }
+
         const auto &key = args[0];
         auto defaultValue = args.size() > 1 ? args[1] : NIL{};
 
@@ -60,15 +77,79 @@ namespace stdlib::DictionaryBuiltins {
         return std::make_shared<Array>(values);
     }
 
+    static Value mergeDictionaries(Value receiver, const std::vector<Value> &args) {
+        const auto receivedObject = receiver.get<std::shared_ptr<Dictionary>>();
+
+        if (!args[0].is<std::shared_ptr<Dictionary>>()) {
+            throw std::runtime_error("expected `Dictionary` but got `" + args[0].type() + "`");
+        }
+
+        const auto otherObject = args[0].get<std::shared_ptr<Dictionary>>();
+        receivedObject->merge(*otherObject);
+        return receivedObject;
+    }
+
+    static Value insertDictionary(Value receiver, const std::vector<Value> &args) {
+        const auto receivedObject = receiver.get<std::shared_ptr<Dictionary>>();
+
+        if (!args[0].is<std::shared_ptr<Dictionary>>()) {
+            throw std::runtime_error("expected `Dictionary` but got `" + args[0].type() + "`");
+        }
+
+        const auto otherObject = args[0].get<std::shared_ptr<Dictionary>>();
+        receivedObject->insert(otherObject->begin(), otherObject->end());
+        return receivedObject;
+    }
+
+    static Value removeDictionaryElementByKey(Value receiver, const std::vector<Value> &args) {
+        const auto receivedObject = receiver.get<std::shared_ptr<Dictionary>>();
+
+        if (!args[0].is<double>() && !args[0].is<std::string>()) {
+            throw std::runtime_error("dictionary key must be number or string");
+        }
+
+        const auto dictionaryElement = receivedObject->extract(args[0]);
+        if (!dictionaryElement) {
+            throw std::runtime_error("dictionary element does not exist");
+        }
+
+        return dictionaryElement.mapped();
+    }
+
+    static Value createDictionaryFromKeys(Value receiver, const std::vector<Value> &args) {
+        const auto receivedObject = receiver.get<std::shared_ptr<Dictionary>>();
+
+        if (!args[0].is<std::shared_ptr<Array>>()) {
+            throw std::runtime_error("expected an array of keys, but got `" + args[0].type() + "`");
+        }
+
+        const auto keysArray = args[0].get<std::shared_ptr<Array>>();
+
+        receivedObject->clear();
+        receivedObject->reserve(keysArray->size());
+
+        for (const auto &key : *keysArray) {
+            receivedObject->emplace(key, args[1]);
+        }
+
+        return receivedObject;
+    }
+
     void registerMethods(std::unordered_map<std::string, std::shared_ptr<NativeFunction>>& registry) {
         registry["len"] = std::make_shared<NativeFunction>("len", 0, getDictionaryLength);
         registry["copy"] = std::make_shared<NativeFunction>("copy", 0, createDictionaryCopy);
         registry["clear"] = std::make_shared<NativeFunction>("clear", 0, clearDictionary);
         registry["isEmpty"] = std::make_shared<NativeFunction>("isEmpty", 0, checkDictionaryEmptiness);
+        registry["contains"] = std::make_shared<NativeFunction>("contains", 1, containsValue);
 
         registry["get"] = std::make_shared<NativeFunction>("get", -1, getDictionaryElementByKey);
         registry["setDefault"] = std::make_shared<NativeFunction>("setDefault", -1, setDictionaryElementByKey);
         registry["keys"] = std::make_shared<NativeFunction>("keys", 0, retrieveDictionaryKeys);
         registry["values"] = std::make_shared<NativeFunction>("values", 0, retrieveDictionaryValues);
+
+        registry["merge"] = std::make_shared<NativeFunction>("merge", 1, mergeDictionaries);
+        registry["insert"] = std::make_shared<NativeFunction>("insert", 1, insertDictionary);
+        registry["pop"] = std::make_shared<NativeFunction>("pop", 1, removeDictionaryElementByKey);
+        registry["fromKeys"] = std::make_shared<NativeFunction>("fromKeys", 2, createDictionaryFromKeys);
     }
 }
