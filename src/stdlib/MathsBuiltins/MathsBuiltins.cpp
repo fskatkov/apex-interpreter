fac3b2842ed089b06e8ccb5432ba549d38af9b8a
Katkov Fedor <pweverj126@gmail.com>
2026-06-19 19:42:30 +0300

    [Update][MathsBuiltins] Added new methods to MathsBuiltins components
    In this update, Maths component contains exp, log, log2, log10, logn functions

--- src/stdlib/MathsBuiltins/MathsBuiltins.cpp
+++ src/stdlib/MathsBuiltins/MathsBuiltins.cpp
@@ -47,6 +47,54 @@ namespace stdlib::MathsBuiltins {
             return std::clamp(args.front().get<double>(), args[1].get<double>(), args[2].get<double>());
         }
 
+        Value take_square_root(const Value &, const std::vector<Value> &args) {
+            if (!args.front().is<double>()) {
+                throw std::invalid_argument(std::format("expected Number but got {}", args.front().type()));
+            }
+
+            return std::sqrt(args.front().get<double>());
+        }
+
+        Value compute_exponent(const Value &, const std::vector<Value> &args) {
+            if (!args.front().is<double>()) {
+                throw std::invalid_argument(std::format("expected Number but got {}", args.front().type()));
+            }
+
+            return std::exp(args.front().get<double>());
+        }
+
+        Value compute_log(const Value &, const std::vector<Value> &args) {
+            if (!args.front().is<double>()) {
+                throw std::invalid_argument(std::format("expected Number but got {}", args.front().type()));
+            }
+
+            return std::log(args.front().get<double>());
+        }
+
+        Value compute_log10(const Value &, const std::vector<Value> &args) {
+            if (!args.front().is<double>()) {
+                throw std::invalid_argument(std::format("expected Number but got {}", args.front().type()));
+            }
+
+            return std::log10(args.front().get<double>());
+        }
+
+        Value compute_log2(const Value &, const std::vector<Value> &args) {
+            if (!args.front().is<double>()) {
+                throw std::invalid_argument(std::format("expected Number but got {}", args.front().type()));
+            }
+
+            return std::log2(args.front().get<double>());
+        }
+
+        Value compute_log_n(const Value &, const std::vector<Value> &args) {
+            if (!args.front().is<double>() || !args[1].is<double>()) {
+                throw std::invalid_argument(std::format("expected Number but got {} and {}", args.front().type(), args[1].type()));
+            }
+
+            return std::log(args.front().get<double>()) / std::log(args[1].get<double>());
+        }
+
     }
 
     std::unordered_map<std::string, std::shared_ptr<NativeFunction> > registerMethods() {
@@ -84,6 +132,48 @@ namespace stdlib::MathsBuiltins {
                     .name = "clamp",
                     .arity = 3,
                     .callable = restrict_value_to_range
+                })
+            },
+            {
+                "sqrt", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "sqrt",
+                    .arity = 1,
+                    .callable = take_square_root
+                })
+            },
+            {
+                "exp", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "exp",
+                    .arity = 1,
+                    .callable = compute_exponent
+                })
+            },
+            {
+                "log", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "log",
+                    .arity = 1,
+                    .callable = compute_log
+                })
+            },
+            {
+                "log10", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "log10",
+                    .arity = 1,
+                    .callable = compute_log10
+                })
+            },
+            {
+                "log2", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "log2",
+                    .arity = 1,
+                    .callable = compute_log2
+                })
+            },
+            {
+                "logn", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "logn",
+                    .arity = 2,
+                    .callable = compute_log_n
                 })
             },
         };
