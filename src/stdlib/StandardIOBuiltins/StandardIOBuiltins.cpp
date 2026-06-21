42db96ccbd476fa48268784a8d51f0a4e3a3e021
Katkov Fedor <pweverj126@gmail.com>
2026-06-22 01:28:00 +0300

    [Feature][StdIO] Added file interface to StdIO
    Despite the fact that I've added methods to work with files to StdIO, I kinda don't like 'em. They look too ugly and don't provide the necessary interface to work with files. Maybe I need to spend some time to add File object and ideally a special interface with a set of methods to update that

--- src/stdlib/StandardIOBuiltins/StandardIOBuiltins.cpp
+++ src/stdlib/StandardIOBuiltins/StandardIOBuiltins.cpp
@@ -2,13 +2,6 @@
 
 namespace stdlib::StandardIOBuiltins {
     namespace {
-        Value builtin_print(const Value &, const std::vector<Value> &args) {
-            for (const auto &arg: args) {
-                std::cout << arg.str();
-            }
-            return NIL{};
-        }
-
         Value builtin_println(const Value &, const std::vector<Value> &args) {
             for (const auto &arg: args) {
                 std::cout << arg.str();
@@ -27,7 +20,7 @@ namespace stdlib::StandardIOBuiltins {
                 return std::make_shared<std::string>(std::move(input));
             }
 
-            return NIL{};
+            throw std::runtime_error("invalid input format");
         }
 
         Value builtin_flush(const Value &, const std::vector<Value> &args) {
@@ -41,6 +34,94 @@ namespace stdlib::StandardIOBuiltins {
             return NIL{};
         }
 
+        String builtin_open_file(const Value &, const std::vector<Value> &args) {
+            if (args.empty()) {
+                throw std::invalid_argument("did not provide file path");
+            }
+
+            if (!args.front().is<String>()) {
+                throw std::runtime_error(std::format("`{}`: No such file or directory", args.front().str()));
+            }
+
+            std::filesystem::path path(*args.front().get<String>());
+
+            if (!std::filesystem::exists(path) || !std::filesystem::is_regular_file(path)) {
+                throw std::runtime_error("file not found or is a directory");
+            }
+
+            std::ifstream file(path, std::ios::in | std::ios::binary);
+            if (!file) {
+                throw std::runtime_error(std::format("failed to read file at path `{}`", path.string()));
+            }
+
+            std::ostringstream buffer;
+            buffer << file.rdbuf();
+
+            return std::make_shared<std::string>(buffer.str());
+        }
+
+        bool builtin_check_file_exists(const Value &, const std::vector<Value> &args) {
+            if (args.empty()) {
+                throw std::invalid_argument("did not provide file path");
+            }
+
+            if (!args.front().is<String>()) {
+                throw std::runtime_error(std::format("`{}`: No such file or directory", args.front().str()));
+            }
+
+            const std::filesystem::path path(*args.front().get<String>());
+            return std::filesystem::exists(path);
+        }
+
+        NIL builtin_write_to_file(const Value &, const std::vector<Value> &args) {
+            if (!args.front().is<String>()) {
+                throw std::runtime_error(std::format("`{}`: No such file or directory", args.front().str()));
+            }
+
+            if (!args[1].is<String>()) {
+                throw std::invalid_argument("invalid data");
+            }
+
+            const std::filesystem::path path(*args.front().get<String>());
+            if (std::ofstream file(path, std::ios::out | std::ios::trunc | std::ios::binary); file) {
+                file << *args[1].get<String>();
+            }
+
+            return NIL{};
+        }
+
+        NIL builtin_append_to_file(const Value &, const std::vector<Value> &args) {
+            if (!args.front().is<String>()) {
+                throw std::runtime_error(std::format("`{}`: No such file or directory", args.front().str()));
+            }
+
+            if (!args[1].is<String>()) {
+                throw std::invalid_argument("invalid data");
+            }
+
+            const std::filesystem::path path(*args.front().get<String>());
+            if (std::ofstream file(path, std::ios::out | std::ios::app | std::ios::binary); file) {
+                file << *args[1].get<String>();
+            }
+
+            return NIL{};
+        }
+
+        bool builtin_remove_file(const Value &, const std::vector<Value> &args) {
+            if (args.empty()) {
+                throw std::invalid_argument("did not provide file path");
+            }
+
+            if (!args.front().is<String>()) {
+                throw std::runtime_error(std::format("`{}`: No such file or directory", args.front().str()));
+            }
+
+            const std::filesystem::path path(*args.front().get<String>());
+
+            std::error_code error_code;
+            const auto &removed = std::filesystem::remove(path, error_code);
+            return removed;
+        }
     }
 
     std::unordered_map<std::string, std::shared_ptr<NativeFunction> > register_methods() {
@@ -72,7 +153,42 @@ namespace stdlib::StandardIOBuiltins {
                     .arity = 0,
                     .callable = builtin_close
                 })
-            }
+            },
+            {
+                "open", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "open",
+                    .arity = 1,
+                    .callable = builtin_open_file
+                })
+            },
+            {
+                "fileExists", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "fileExists",
+                    .arity = 1,
+                    .callable = builtin_check_file_exists
+                })
+            },
+            {
+                "write", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "write",
+                    .arity = 2,
+                    .callable = builtin_write_to_file
+                })
+            },
+            {
+                "append", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "append",
+                    .arity = 2,
+                    .callable = builtin_append_to_file
+                })
+            },
+            {
+                "removeFile", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "removeFile",
+                    .arity = 1,
+                    .callable = builtin_remove_file
+                })
+            },
         };
     }
 }
