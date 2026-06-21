1439274674f761a5d24e4bfdd216f34b31d7f258
Katkov Fedor <pweverj126@gmail.com>
2026-06-22 01:02:34 +0300

    [Feature][StdIO] Started expanding StdIO component in stdlib
    Added readLine, flush and close methods to StdIO component in stdlib. Now the magic begins...

--- src/stdlib/StandardIOBuiltins/StandardIOBuiltins.cpp
+++ src/stdlib/StandardIOBuiltins/StandardIOBuiltins.cpp
@@ -2,6 +2,13 @@
 
 namespace stdlib::StandardIOBuiltins {
     namespace {
+        Value builtin_print(const Value &, const std::vector<Value> &args) {
+            for (const auto &arg: args) {
+                std::cout << arg.str();
+            }
+            return NIL{};
+        }
+
         Value builtin_println(const Value &, const std::vector<Value> &args) {
             for (const auto &arg: args) {
                 std::cout << arg.str();
@@ -9,6 +16,31 @@ namespace stdlib::StandardIOBuiltins {
             std::cout << std::endl;
             return NIL{};
         }
+
+        Value builtin_read_line(const Value &, const std::vector<Value> &args) {
+            if (!args.empty() && args.front().is<String>()) {
+                std::cout << *args.front().get<String>();
+                std::cout.flush();
+            }
+
+            if (std::string input; std::getline(std::cin, input)) {
+                return std::make_shared<std::string>(std::move(input));
+            }
+
+            return NIL{};
+        }
+
+        Value builtin_flush(const Value &, const std::vector<Value> &args) {
+            std::cout.flush();
+            return NIL{};
+        }
+
+        Value builtin_close(const Value &, const std::vector<Value> &args) {
+            std::cin.rdbuf(nullptr);
+            std::cout.rdbuf(nullptr);
+            return NIL{};
+        }
+
     }
 
     std::unordered_map<std::string, std::shared_ptr<NativeFunction> > register_methods() {
@@ -18,6 +50,27 @@ namespace stdlib::StandardIOBuiltins {
                     .name = "println",
                     .arity = -1,
                     .callable = builtin_println
+                })
+            },
+            {
+                "readLine", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "readLine",
+                    .arity = -1,
+                    .callable = builtin_read_line
+                })
+            },
+            {
+                "flush", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "flush",
+                    .arity = 0,
+                    .callable = builtin_flush
+                })
+            },
+            {
+                "close", std::make_shared<NativeFunction>(NativeFunction{
+                    .name = "close",
+                    .arity = 0,
+                    .callable = builtin_close
                 })
             }
         };
